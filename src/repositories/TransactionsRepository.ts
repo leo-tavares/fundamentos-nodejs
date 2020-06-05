81c8704ad538b5df79534e37e3b07bc23c484567
leo-tavares <leonardotavaresdev@gmail.com>
2020-06-05 02:20:49 -0300

    feat: Create transaction List transaction and balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (acc: Balance, transaction) => {
+        const transactionType = transaction.type;
+        if (transactionType === 'income') {
+          acc.income += transaction.value;
+        } else if (transactionType === 'outcome') {
+          acc.outcome += transaction.value;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
